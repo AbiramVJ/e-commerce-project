c4126945ee570a5ed618dde52ddbba16421c9375
V.Abiram <70306664+AbiramVJ@users.noreply.github.com>
2022-01-14 11:54:34 +0530

    user dedux setUp

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//redux
+import Provider from 'react-redux';
+import {BrowserRouter} from "react-router-dom";
 
-
-ReactDOM.render(
-  <React.StrictMode>
+ReactDOM.render( 
+  <Provider>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
